753c576e4d3607deb70a1fb2f6e20f814f81d998
MarkJomar <markjomarcalmateo@email.com>
2025-11-05 03:26:42 +0800

    designing UI

--- header_app.php
+++ header_app.php
@@ -67,8 +67,13 @@ if (in_array($role, $roles_that_need_profile) && $profile_incomplete && !in_arra
 <html lang="en">
 <head>
     <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="app_style.css"> <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    </head>
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+    <link rel="stylesheet" href="app_style.css"> 
+</head>
 <body>
     <header class="top-navbar">
         <div class="navbar-container">
